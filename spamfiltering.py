f97e5bf7bfa13f50e46523c6aa8c2c4cd4ea999e
Yolo Jolo <jolosimeon@users.noreply.github.com>
2016-12-14 13:26:38 +0800

    fixed

--- spamfiltering.py
+++ spamfiltering.py
@@ -6,7 +6,7 @@ from random import randint
 def load_files():
     email_dir = 'emails/bare'
     # 10-fold cross validation means repeat 10 times: divide data into 10, use 9 for training, 1 for testing
-    test_folder = randint(1, 10)
+    test_folder = 1 #randint(1, 10)
     print test_folder
     for num in range(1, 11):
         if num != test_folder:
@@ -14,15 +14,16 @@ def load_files():
             part_folder = '/part' + str(part_num)
             print part_folder
             for filename in os.listdir(email_dir + part_folder):
-                type = 'ham'
+                email_type = 'ham'
                 if 'sp' in filename:
-                    type = 'spam'
+                    email_type = 'spam'
                 email_file = open(email_dir + part_folder + "/" + filename, "r")
                 content = email_file.readlines()
-                train(content, type)
+                train(content, email_type)
                 #print email_file
 
-def train(content. type):
+
+def train(content, email_type):
     print 'choo choo motherfucker'
 
 load_files()
